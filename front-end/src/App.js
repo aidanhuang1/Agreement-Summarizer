a834c35a65a7f7fffdece8cb746c79b8c7e36a80
Aidan Huang <aidanhuangsiqi@gmail.com>
2021-09-18 01:06:50 -0400

    update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import "./App.css";
 import NavbarPage from "./components/Header/NavbarPage";
 import BigPicture from "./components/Header/BigPicture";
@@ -11,6 +12,28 @@ function App() {
       <div>
         
       </div>
+=======
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> parent of d91df67 (Add TailwindCSS)
     </div>
   );
 }
