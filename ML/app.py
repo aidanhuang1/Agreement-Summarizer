0b002547a6a2470c9b574834a5da5aaddbcd5063
Aidan Huang <aidanhuangsiqi@gmail.com>
2021-09-18 20:06:16 -0400

    Install Flask

--- ML/app.py
+++ ML/app.py
@@ -3,4 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', methods=['GET']) 
-def 
+def hello_world():
+    return "hello"
+
+if __name__ == '__main__':
+    app.run(debug=True)
